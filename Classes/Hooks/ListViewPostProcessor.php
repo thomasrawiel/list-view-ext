5ece46555f5f679a7a7dd166f5816296487628a4
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-11-17 15:19:17 +0100

    [feat] use LanguageService to get checked/ unchecked labels

--- Classes/Hooks/ListViewPostProcessor.php
+++ Classes/Hooks/ListViewPostProcessor.php
@@ -3,8 +3,16 @@ declare(strict_types=1);
 
 namespace TRAW\ListViewExt\Hooks;
 
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+
 class ListViewPostProcessor
 {
+    public function __construct(
+        private readonly LanguageServiceFactory $languageServiceFactory,
+    ) {}
+
     public function postProcessValue(array $params): string
     {
         $value = $params['value'] ?? '';
@@ -13,13 +21,29 @@ class ListViewPostProcessor
         if (($conf['renderType'] ?? null) !== 'checkboxLabeledToggle') {
             return $value;
         }
-        $labelChecked = $conf['items'][0]['labelChecked'] ?? 'Enabled';
-        $labelUnchecked = $conf['items'][0]['labelUnchecked'] ?? 'Disabled';
+        $labelChecked = $this->translate($conf['items'][0]['labelChecked'] ?? 'Enabled');
+        $labelUnchecked = $this->translate($conf['items'][0]['labelUnchecked'] ?? 'Disabled');
 
         if ($value === 'Yes' || $value === '1' || $value === 1) {
             return $labelChecked;
         }
 
         return $labelUnchecked;
+    }
+
+    private function translate(string $input): string
+    {
+        return $this->getLanguageService()->sL($input);
+    }
+
+    private function getLanguageService(): LanguageService
+    {
+        return $this->languageServiceFactory
+            ->createFromUserPreferences($this->getBackendUserAuthentication());
+    }
+
+    private function getBackendUserAuthentication(): BackendUserAuthentication
+    {
+        return $GLOBALS['BE_USER'];
     }
 }
